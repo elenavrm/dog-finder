4466a8ccb25f6cb4d1cb1f3bc23942cb145ee403
elenavrm <ellena.vrm@gmail.com>
2025-01-30 17:55:39 -0600

    made some updates

--- src/Pages/Hero/Hero.js
+++ src/Pages/Hero/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './Hero.module.scss'; // Import the module styles
+import styles from './Hero.module.scss'; 
 
 const Hero = () => {
   return (
-    <div className={styles.mainPage}> {/* Apply styles using the imported 'styles' object */}
+    <div className={styles.mainPage}> 
       <h1>Hey! Let's find your perfect dog</h1>
       <p>Browse through our shelter dog database and choose your favorite companions!</p>
     </div>
